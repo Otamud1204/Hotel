1e61dc51caf307ee09a20c1b5366522ca1ae09c0
Azizbek <ismoilovazizbek191004@gmail.com>
2024-07-07 13:13:36 +0500

    out

--- src/main/java/org/uz/entiti/Database/DB.java
+++ src/main/java/org/uz/entiti/Database/DB.java
@@ -8,6 +8,6 @@ import java.util.Scanner;
 public class DB {
     public static Scanner scanner = new Scanner(System.in);
     public static Scanner strScanner = new Scanner(System.in);
-    ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<>();
 
 }
